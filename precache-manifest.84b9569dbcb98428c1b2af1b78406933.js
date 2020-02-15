a6a89518db88320693435615b47da7cb3313cdaa
Kevin Robertson <contact@digitalsquid.net>
2020-02-15 00:29:00 +0000

    Updates

---
+++ precache-manifest.84b9569dbcb98428c1b2af1b78406933.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "649a8180a596d191a0f44f7c7422b316",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "fe4aa12d2b4b5c597efd",
+    "url": "/monsters-rolodex/static/css/main.b4e913e2.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe4aa12d2b4b5c597efd",
+    "url": "/monsters-rolodex/static/js/main.2884665d.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
